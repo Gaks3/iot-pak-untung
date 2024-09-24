112fe7879785128c93cef174727a88d51a139978
Gaks3 <dwi66116@gmail.com>
2024-09-24 23:22:18 +0700

    fix: add bcrypt to webpack config

--- next.config.js
+++ next.config.js
@@ -10,7 +10,7 @@ await import("./src/env.js");
 /** @type {import("next").NextConfig} */
 const config = {
   webpack: (config) => {
-    config.externals.push("@node-rs/argon2");
+    config.externals.push("@node-rs/argon2", "@node-rs/bcrypt");
 
     return config;
   },
