f1a71fae59327ebd1a254011c03f9ae75e78ac5f
Gaks3 <dwi66116@gmail.com>
2024-09-24 22:28:39 +0700

    feat: lucia instance

---
+++ src/server/auth.ts
@@ -0,0 +1,79 @@
+import { PrismaAdapter } from "@lucia-auth/adapter-prisma";
+import { Lucia, TimeSpan, type Session, type User } from "lucia";
+import { db } from "./db";
+import { env } from "@/env";
+import { cache } from "react";
+import { cookies } from "next/headers";
+
+const adapter = new PrismaAdapter(db.session, db.user);
+
+export const lucia = new Lucia(adapter, {
+  sessionCookie: {
+    name: "iot_token",
+    attributes: {
+      secure: env.NODE_ENV === "production",
+    },
+  },
+  sessionExpiresIn: new TimeSpan(1, "w"),
+  getUserAttributes: (attributes) => {
+    return {
+      id: attributes.id,
+      usernamem: attributes.username,
+      email: attributes.email,
+      isAdmin: attributes.isAdmin,
+    };
+  },
+});
+
+declare module "lucia" {
+  interface Register {
+    Lucia: typeof lucia;
+    DatabaseUserAttributes: DatabaseUserAttributes;
+  }
+}
+
+interface DatabaseUserAttributes {
+  id: string;
+  username: string;
+  email: string;
+  isAdmin: boolean;
+}
+
+export const validateRequest = cache(
+  async (): Promise<
+    { user: User; session: Session } | { user: null; session: null }
+  > => {
+    const sessionId = cookies().get(lucia.sessionCookieName)?.value ?? null;
+    if (!sessionId) {
+      return {
+        user: null,
+        session: null,
+      };
+    }
+
+    const result = await lucia.validateSession(sessionId);
+
+    try {
+      if (result.session?.fresh) {
+        const sessionCookie = lucia.createSessionCookie(result.session.id);
+        cookies().set(
+          sessionCookie.name,
+          sessionCookie.value,
+          sessionCookie.attributes,
+        );
+      }
+      if (!result.session) {
+        const sessionCookie = lucia.createBlankSessionCookie();
+        cookies().set(
+          sessionCookie.name,
+          sessionCookie.value,
+          sessionCookie.attributes,
+        );
+      }
+    } catch (error) {
+      console.log(error);
+    }
+
+    return result;
+  },
+);
